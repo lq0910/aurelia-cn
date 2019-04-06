8daa89121d5f45c0d0df2c650a7defdbe8a11eac
lq0910 <960661824@qq.com>
2019-04-06 18:04:23 +0800

    初始化文档项目，使用vuepress来构建文档，文档基本结构初始化。

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  title: "Aurelia 中文网",
+  description: "Aurelia 简单强大的下一代MVVM应用框架",
+  head: [["link", { rel: "icon", href: "/logo.svg" }]],
+  //theme: "@vuepress/vue",
+  themeConfig: {
+    repo: "lq0910/aurelia-cn",
+    repoLabel: "查看源码",
+
+    // 以下为可选的编辑链接选项
+
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    docsRepo: "vuejs/vuepress",
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: "docs",
+    // 假如文档放在一个特定的分支下：
+    docsBranch: "master",
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: "帮助我们改善此页面！",
+    nav: [
+      { text: "起步", link: "/" },
+      { text: "指南", link: "/guide/" },
+      { text: "API", link: "/api/" },
+      {
+        text: "官网通道",
+        items: [
+          { text: "英文文档", link: "https://aurelia.io" },
+          { text: "官方论坛", link: "https://discourse.aurelia.io/" },
+          { text: "官方博客", link: "https://aurelia.io/blog" },
+        ]
+      },
+    ],
+    sidebar: "auto", //自动生成侧栏
+    displayAllHeaders: true, // 默认值：false 显示所有页面的标题链接
+    lastUpdated: "Last Updated", // string | boolean
+    serviceWorker: {
+      updatePopup: true // Boolean | Object, 默认值是 undefined.
+      //如果设置为 true, 默认的文本配置将是:
+      /*  updatePopup: {
+        message: "文档有新的更新",
+        buttonText: "刷新"
+      } */
+    }
+  }
+};
---
+++ docs/README.md
@@ -0,0 +1,30 @@
+---
+home: true
+heroImage: /aurelia-icon.svg
+actionText: 快速上手 →
+actionLink: /zh/guide/
+features:
+  - title: 简单易学
+    details: Aurelia基于标准，不引人注目的风格使其成为唯一一个使用vanilla JavaScript或TypeScript构建组件的框架。如果您了解现代JS和HTML，那么即使是最复杂的应用程序也无法学习。
+  - title: 高性能
+    details: Aurelia的核心是一个高性能的反应系统，能够以一种让其他框架及其虚拟DOM处于灰尘状态的方式批量处理DOM更新。无论您的UI多么复杂，都可以体验一致，可扩展的性能。
+
+
+  - title: 广泛的生态系统
+    details: 国际化和验证 - 核心团队的所有官方插件。CLI，VS Code插件和Chrome调试器 - 用于增强开发的可选工具。Aurelia不仅仅是一个框架，而是一个强大的平台和生态系统，旨在确保您的快速成功。
+  - title: 双向数据绑定
+    details: Aurelia可以对任何对象进行强大的数据绑定。通过使用自适应技术，Aurelia选择最有效的方式来观察模型中的每个属性，并自动将您的UI和状态与最佳性能同步。
+    
+
+  - title: 简单测试
+    details: 通过将现代JS模块与不引人注目的方法相结合，Aurelia使单元测试与测试vanilla JS一样简单。需要编写集成测试吗？强大的依赖注入容器和测试库使其变得快速而简单。您可以从高度可维护和更长寿的应用程序中受益。
+  - title: 超强的可扩展性
+    details: 业内没有任何东西可以与Aurelia的可扩展性相媲美。您可以创建自定义元素，向现有元素添加自定义属性，控制模板生成，自定义模板语法，创建新的反应绑定类型，扩展DI以及您能想到的任何其他内容。
+
+
+  - title: 使用约定的轻松编码
+    details: 约定使您可以专注于您的代码和业务逻辑。Aurelia是当今唯一的框架，它将您的代码置于应用程序的中心，而不是框架。不喜欢这些惯例？插上你自己的或完全丢弃它们。你在掌控之中。
+  - title: 路由，组合和渐进增强
+    details: 利用Aurelia先进的客户端路由器及其可插拔管道和子路由器。不需要路由器但需要动态的，数据驱动的UI组合？我们也这样做。有遗留应用吗？Aurelia的渐进增强和脚本标记构建使得逐步采用也变得容易。
+footer: MIT Licensed | Copyright © 2019-present Aurelia Chinese
+---
