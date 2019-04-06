0abaecd42600ba51e9ecd8426275693a64339033
lq0910 <960661824@qq.com>
2019-04-06 23:43:40 +0800

    补充提交；

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,9 +2,9 @@ module.exports = {
   title: "Aurelia 中文网",
   description: "Aurelia 简单强大的下一代MVVM应用框架",
   head: [
-    ["link", { rel: "icon", href: "/icon.png" }]
+    ["link", { rel: "icon", href: "/icons/icon.png" }]
   ],
-  base: '/', // 设置站点根路径
+  //base: '/', // 设置站点根路径
   //theme: "@vuepress/vue",
   themeConfig: {
     repo: "lq0910/aurelia-cn",
@@ -15,9 +15,9 @@ module.exports = {
     // 假如你的文档仓库和项目本身不在一个仓库：
     //docsRepo: "lq0910/aurelia-cn",
     // 假如文档不是放在仓库的根目录下：
-    docsDir: "docs",
+    docsDir: "/docs",
     // 假如文档放在一个特定的分支下：
-    docsBranch: "master",
+    //docsBranch: "master",
     // 默认是 false, 设置为 true 来启用
     editLinks: true,
     // 默认为 "Edit this page"
@@ -37,7 +37,7 @@ module.exports = {
     ],
     sidebar: "auto", //自动生成侧栏
     displayAllHeaders: true, // 默认值：false 显示所有页面的标题链接
-    lastUpdated: "Last Updated", // string | boolean
+    lastUpdated: "上次更新", // string | boolean
     serviceWorker: {
       updatePopup: true // Boolean | Object, 默认值是 undefined.
       //如果设置为 true, 默认的文本配置将是:
