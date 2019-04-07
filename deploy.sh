5199f328a3ed2a8856415fb434f3bdc75a758cf3
lq0910 <960661824@qq.com>
2019-04-07 21:04:49 +0800

    仓库整合；

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.aurelia.pub' > CNAME
+echo 'aurelia.pub' > CNAME
 
 git init
 git add -A
@@ -21,5 +21,4 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:lq0910/aurelia-cn.git master:gh-pages
-
 cd -
